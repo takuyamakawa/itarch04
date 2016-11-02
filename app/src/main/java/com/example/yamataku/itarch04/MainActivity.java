03b88f036ef68b0ade268c24f72e2e68f8916fe9
takuyamakawa <b1013167@fun.ac.jp>
2016-11-02 11:49:50 +0900

    aidlファイル追加

--- app/src/main/java/com/example/yamataku/itarch04/MainActivity.java
+++ app/src/main/java/com/example/yamataku/itarch04/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    IServiceMethod aidl = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
