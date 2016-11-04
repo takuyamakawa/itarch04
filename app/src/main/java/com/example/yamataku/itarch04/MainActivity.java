b8ebdd8f7ff2b5884609659a1bc0424231e5fe63
takuyamakawa <b1013167@fun.ac.jp>
2016-11-05 02:11:12 +0900

    サービス実行成功

--- app/src/main/java/com/example/yamataku/itarch04/MainActivity.java
+++ app/src/main/java/com/example/yamataku/itarch04/MainActivity.java
@@ -6,21 +6,25 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import static android.content.Context.BIND_AUTO_CREATE;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity {
 
-    IMyAidlInterface aidl = null;
-    TextView res = new TextView(this);
-    Button btn = new Button(this);
+    IMyAidlInterface aidl;
+    TextView res;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // activity_main.xml にUIコンポーネントを配置する
         setContentView(R.layout.activity_main);
 
         // SubServiceにbindする
@@ -29,16 +33,37 @@ public class MainActivity extends Activity implements View.OnClickListener {
         bindService(i, serviceConnection, BIND_AUTO_CREATE);
 
         btn = (Button) findViewById(R.id.start);
-        btn.setOnClickListener(this);//リスナの登録
+        //btn.setOnClickListener(this);//リスナの登録
 
-        btn  = (Button) findViewById(R.id.stop);
-        btn.setOnClickListener(this);//リスナの登録
-
-        // activity_main.xml にUIコンポーネントを配置する
-        setContentView(R.layout.activity_main);
+        //btn  = (Button) findViewById(R.id.stop);
+        //btn.setOnClickListener(this);//リスナの登録
 
         res = (TextView) findViewById(R.id.calcResult);
 
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                switch(v.getId()){
+
+                    case R.id.start://startServiceでサービスを起動
+                        //TextView res = (TextView) findViewById(R.id.calcResult);
+
+                        int result = 0;
+                        try {
+                            result = aidl.add(1, 2);
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                        res.setText(String.valueOf(result));
+
+                        break;
+//                    case R.id.stop://stopServiceでサービスの終了
+//                        stopService(new Intent(MainActivity.this, SubService.class));
+//                        break;
+                }
+            }
+        });
     }
 
     @Override
@@ -51,25 +76,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
 
 
-
-    btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-
-                switch(v.getId()){
-
-                    case R.id.start://startServiceでサービスを起動
-                        //TextView res = (TextView) findViewById(R.id.calcResult);
-
-                        int result = aidl.add(1, 2);
-                        res.setText(result);
-
-                        break;
-                    case R.id.stop://stopServiceでサービスの終了
-                        stopService(new Intent(MainActivity.this, SubService.class));
-                        break;
-                }
-            }
-        };
 
     private ServiceConnection serviceConnection = new ServiceConnection()
     {
